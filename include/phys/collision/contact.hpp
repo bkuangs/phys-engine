8b599799c4c4d76ba50931f7fd4d436828962431
kuangbrian71@gmail.com <bkuangs@DESKTOP-KMK69EU.localdomain>
2026-09-14 09:38:56 -0400

    contact manifolds

--- include/phys/collision/contact.hpp
+++ include/phys/collision/contact.hpp
@@ -1,7 +1,18 @@
 #pragma once
+#include "phys/math/vec3.hpp"
 
 namespace phys {
 
-struct ContactPoint;
+struct ContactPoint
+{
+	Vec3 localAnchorA{};
+	Vec3 localAnchorB{};
+
+	float penetration = 0.0f;
+
+	float normalImpulse = 0.0f;
+	float tangentImpulse1 = 0.0f;
+	float tangentImpulse2 = 0.0f;
+};
 
 }
--- include/phys/collision/manifold.hpp
+++ include/phys/collision/manifold.hpp
@@ -1,7 +1,24 @@
 #pragma once
+#include <array>
+#include <cstdint>
+#include "phys/collision/contact.hpp"
+#include "phys/world/body_handle.hpp"
 
 namespace phys {
 
-struct ContactManifold;
+struct ContactManifold
+{
+	RigidBodyHandle bodyA{};
+	RigidBodyHandle bodyB{};
+
+	// Points from body A toward body B.
+	Vec3 normal{};
+
+	std::array<ContactPoint, 4> points{};
+	uint32_t pointCount = 0;
+
+	float friction = 0.0f;
+	float restitution = 0.0f;
+};
 
 }
